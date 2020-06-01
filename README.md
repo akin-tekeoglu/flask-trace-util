70a770d1dec8ca37f326d37093ab5528ed6bb7d6
akin-tekeoglu <akin.tekeoglu@gmail.com>
2020-06-01 14:46:54 +0300

    update project name

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Flask Trace
+
+`export PIPENV_VENV_IN_PROJECT="enabled" && pipenv install --pre --dev`
+
+# Deployment
+
+`pipenv run python setup.py sdist bdist_wheel`
+
+`pipenv run python -m twine upload dist/*`
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="flask-trace",
+    name="flask-trace-util",
     version="0.0.1",
     author="Akın Tekeoğlu",
     author_email="akin.tekeoglu@gmail.com",
